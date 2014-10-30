8e35b2e92ba9f86366c91235e94e10014e7dabaa
Bryant M <web@codeddesign.org>
2014-10-30 13:12:28 +0200

    ....

--- tests/create_project.php
+++ tests/create_project.php
@@ -1,19 +1,42 @@
 <?php
-require_once 'autoload.php';
+/**
+ * @param $className
+ */
+function __autoload($className)
+{
+    $dirs = array(
+        'helpers',
+        'libraries',
+        'services',
+
+    );
+
+    foreach ($dirs as $d_no => $dir) {
+        $fileName = '../' . $dir . '/' . $className . '.php';
+        if (file_exists($fileName)) {
+            require_once $fileName;
+        }
+    }
+}
 
 // load time of the script:
 $ld = new LoadTime();
 
+if (!isset($_POST['maxDepth']) OR $_POST['maxDepth'] == '0' OR $_POST['atOnce'] == 0) {
+    header('Location: index.php?err=1');
+    exit();
+}
+
 $rules = array(
-    'atOnce' => '10',
-    'botName' => 'boomlink.de',
-    'maxDepth' => '3',
+    'atOnce' => trim($_POST['atOnce']),
+    'botName' => trim($_POST['botName']),
+    'maxDepth' => trim($_POST['maxDepth']),
 );
 
 $project = array(
     // new:
-    'project_title' => 'ran-x',
-    'url' => 'http://ran.de',
+    'project_title' => $_POST['project_title'],
+    'url' => trim($_POST['url']),
     'config' => json_encode($rules, true),
 );
 $domain = Standards::getHost($project['url']);
@@ -30,3 +53,6 @@ $domain_id = $db->runQuery($q);
 // save main link:
 $q = 'INSERT INTO _sitemap_links (domain_id, page_url) VALUES (\'' . $domain_id . '\', \'' . $clean_url . '\')';
 $link_id = $db->runQuery($q);
+
+HERE:
+header('Location: index.php?msg=1');
---
+++ tests/index.php
@@ -0,0 +1,23 @@
+<div style="background-color: yellow;color: red;"><?php if (isset($_GET['msg'])) echo 'Your project was saved'; ?></div>
+<form method="POST" action="create_project.php">
+    <input type="text" placeholder="bot-name" name="botName">
+    <select name="atOnce">
+        <option value="0">links at once</option>
+        <OPTION VALUE="3">3</OPTION>
+        <OPTION VALUE="5">5</OPTION>
+        <OPTION VALUE="10">10</OPTION>
+        <OPTION VALUE="4">4</OPTION>
+        <OPTION VALUE="5">5</OPTION>
+    </select>
+    <select name="maxDepth">
+        <option value="0">depth</option>
+        <OPTION VALUE="1">1</OPTION>
+        <OPTION VALUE="2">2</OPTION>
+        <OPTION VALUE="3">3</OPTION>
+        <OPTION VALUE="4">4</OPTION>
+        <OPTION VALUE="5">5</OPTION>
+    </select>
+    <input type="text" placeholder="project's title" name="project_title">
+    <input type="text" placeholder="main url" name="url">
+    <input type="submit" value="ADD">
+</form>
