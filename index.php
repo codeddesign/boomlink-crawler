52aa1904f8dba6fccd6e0312487f66965e83c8c1
Bryant M <web@codeddesign.org>
2014-12-02 05:56:07 +0200

    * created external controller for crawler service - now it can be checked for it's existance - it can be stoped/started and also check for status (running or not)

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * index.php
+ * - acts like an external controller
+ * - accepts a limited case of operations
+ * - if no operation is set it exists stating that the controller 'exists'
+ * */
+
+$operations = array(
+    'start',
+    'stop',
+    'status'
+);
+
+$op = 'n/a';
+$msg = 'exists';
+if (isset($_GET['op'])) {
+    $op = strtolower(trim($_GET['op']));
+}
+
+function getRunning()
+{
+    $running = array();
+    $lines = explode("\n", shell_exec('ps aux | grep \'php run.php\''));
+    foreach ($lines as $l_no => $line) {
+        if (trim($line) and stripos($line, 'grep') === false) {
+            // remove 2 spaces:
+            $line = str_replace('  ', ' ', $line);
+
+            // split the line:
+            $parts = explode(' ', $line);
+            $total = count($parts);
+
+            // 'form' command:
+            $command = trim($parts[$total - 2] . ' ' . $parts[$total - 1]);
+            if ($command == 'php run.php') {
+                $running[] = $parts[1];
+            }
+        }
+    }
+
+    return $running;
+}
+
+/*
+ * responses:
+ * - started / stopped
+ * - on / off
+ * - killed
+*/
+switch ($op) {
+    case 'start':
+        $msg = 'already';
+        if (!count(getRunning())) {
+            shell_exec('php run.php > /dev/null &');
+            $msg = 'started';
+        }
+
+        break;
+    case 'stop':
+        $pids = getRunning();
+
+        $msg = 'already';
+        if (count($pids)) {
+            shell_exec('kill -9 ' . implode(' ', $pids));
+            $msg = 'killed';
+        }
+        break;
+    case 'status':
+        $pids = getRunning();
+
+        $msg = 'on';
+        if (!count($pids)) {
+            $msg = 'off';
+        }
+        break;
+}
+
+exit($msg);
